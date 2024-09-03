8bb69a4639ca6106cd26594152dad5f2857b893c
kimkganghyun <163385332+kimkganghyun@users.noreply.github.com>
2024-09-04 01:35:16 +0900

    🐛 버그 수정 : 수정 완료 게임 페이지 넘어가게로직

--- kmbbj/src/routes/GameRoutes.js
+++ kmbbj/src/routes/GameRoutes.js
@@ -5,12 +5,10 @@ import GameResult from '../components/Games/GameResult';
 import GameOver from '../pages/Games/GameOverPage';
 import UserAssetPage from '../pages/Games/UserAssetPage';  
 import TransactionHistoryPage from '../pages/Games/TransactionHistoryPage';  
-import GameStartPage from '../pages/Games/GameStartPage';
 
 const GameRoutes = () => {
   return (
     <Routes>
-      <Route path="/start/:data" element={<GameStartPage />} />
       <Route path="status/:gameId/balance/:userId" element={<GamePage />} />
       <Route path="result/:encryptedGameId" element={<GameResult />} />
       <Route path="GameOver/:encryptedGameId" element={<GameOver />} />
