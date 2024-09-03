284d8bf0430fc7ab00ab1b1530c6daad19dfd1fc
Jeayoung <jjy0843@gmail.com>
2024-09-04 03:50:55 +0900

    ♻️ 코드 리팩토링 : 오류 메세지 전달 및 오류 발생 시 페이지 이동

--- kmbbj/src/components/Matching/RoomDetail.js
+++ kmbbj/src/components/Matching/RoomDetail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import moment from "moment";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../api/api";
 import "../../assets/styles/Matching/RoomDetail.css";
@@ -8,20 +9,61 @@ function RoomDetail() {
   const navigate = useNavigate();
   const [room, setRoom] = useState(null);
   const [error, setError] = useState(null);
+  const [countdown, setCountdown] = useState("");
 
   useEffect(() => {
     const fetchRoomData = async () => {
       try {
         const response = await api.post(`/room/enter/${roomId}`);
         setRoom(response.data.data);
-      } catch (err) {
-        console.error("Failed to fetch room data:", err);
-        setError("Failed to load room data");
+        const gameStartTime = new Date(Date.now() + response.data.data * 60000);
+        localStorage.setItem("gameStartTime", gameStartTime);
+        initializeCountdown(gameStartTime);
+      } catch (error) {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.exception
+        ) {
+          // 백엔드에서 전달된 오류 메시지를 표시
+          alert(`${error.response.data.exception.errorMessage}`);
+        } else {
+          // 기타 네트워크 또는 예상치 못한 오류 메시지 표시
+          alert("방을 입장 중 오류가 발생했습니다.");
+          window.location.href = `/matching/list`;
+        }
+        console.error("Failed to enter room:", error);
       }
     };
 
     fetchRoomData();
   }, [roomId]);
+
+  const initializeCountdown = (gameStartTime) => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      const difference = gameStartTime - now;
+
+      if (difference > 0) {
+        const minutes = Math.floor(
+          (difference % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        setCountdown(`${minutes}분 ${seconds}초 남음`);
+      } else {
+        setCountdown("게임이 시작되었습니다.");
+        clearInterval(timer);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  };
+
+  useEffect(() => {
+    const storedStartTime = localStorage.getItem("gameStartTime");
+    if (storedStartTime) {
+      initializeCountdown(new Date(storedStartTime));
+    }
+  }, []);
 
   if (error) {
     return <div>{error}</div>;
@@ -40,24 +82,48 @@ function RoomDetail() {
         console.error("Failed to leave room");
         setError("Failed to leave room");
       }
-    } catch (err) {
-      console.error("Failed to leave room:", err);
-      setError("Failed to leave room");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.exception
+      ) {
+        // 백엔드에서 전달된 오류 메시지를 표시
+        alert(`${error.response.data.exception.errorMessage}`);
+      } else {
+        // 기타 네트워크 또는 예상치 못한 오류 메시지 표시
+        alert("방을 나가던 중 오류가 발생했습니다.");
+      }
+      console.error("Failed to leave room:", error);
     }
   };
 
   const handleStartGame = async () => {
     try {
       const response = await api.post(`/room/start/${roomId}`);
-      if (response.data.status === "OK") {
+      if (response.data.status === "OK" && response.data.data !== 0) {
         console.log("Game started successfully");
+        const currentTime = moment().format("HH:mm");
+        alert(
+          `현재 시간: ${currentTime}을 기준으로 ${response.data.data}분 뒤에 게임이 시작됩니다.`
+        );
       } else {
         console.error("Failed to start game");
         setError("Failed to start game");
       }
-    } catch (err) {
-      console.error("Failed to start game:", err);
-      setError("Failed to start game");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.exception
+      ) {
+        // 백엔드에서 전달된 오류 메시지를 표시
+        alert(`${error.response.data.exception.errorMessage}`);
+      } else {
+        // 기타 네트워크 또는 예상치 못한 오류 메시지 표시
+        alert("게임을 시작하던 중 오류가 발생했습니다.");
+      }
+      console.error("Failed to start game:", error);
     }
   };
 
@@ -72,6 +138,37 @@ function RoomDetail() {
     return `${totalAsset}만원`;
   };
 
+  const formatStartSeedMoney = (startSeedMoney) => {
+    let seedMoneyValue = "";
+    switch (startSeedMoney) {
+      case "THREE_MILLION":
+        seedMoneyValue = "300만";
+        break;
+      case "FIVE_MILLION":
+        seedMoneyValue = "500만";
+        break;
+      case "SEVEN_MILLION":
+        seedMoneyValue = "700만";
+        break;
+      case "TEN_MILLION":
+        seedMoneyValue = "1000만";
+        break;
+      case "FIFTEEN_MILLION":
+        seedMoneyValue = "1500만";
+        break;
+      case "TWENTY_MILLION":
+        seedMoneyValue = "2000만";
+        break;
+      case "THIRTY_MILLION":
+        seedMoneyValue = "3000만";
+        break;
+      default:
+        seedMoneyValue = "??";
+        return;
+    }
+    return seedMoneyValue;
+  };
+
   return (
     <div className="room-detail-container">
       <div className="room-header">
@@ -79,7 +176,11 @@ function RoomDetail() {
           <h2>{room.roomTitle}</h2>
         </div>
         <div className="room-stats">
+          <div>매칭 시작까지 남은 시간: {countdown}</div>
           <p>참가인원: {room.userCount}/10</p>
+          <p>시드 머니: {formatStartSeedMoney(room.startSeedMoney)}</p>
+          <p>딜레이: {room.delay}분</p>
+          <p>라운드: {room.end}</p>
           <p>평균 자산: {formatMoney(room.averageAsset)}</p>
         </div>
         <button className="leave-button" onClick={handleLeaveRoom}>
