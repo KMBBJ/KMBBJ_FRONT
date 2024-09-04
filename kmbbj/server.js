1658c74859054df4d050b0637bbe8e780d60341f
minseoBae <baejjae8218@gmail.com>
2024-09-04 21:30:16 +0900

    ⚙️ 설정사항 수정: server.js 파일변경

--- kmbbj/server.js
+++ kmbbj/server.js
@@ -3,10 +3,31 @@ const express = require('express');
 const cors=require("cors");
 const app = express();
 
+const APP_FRONT_URL = process.env.REACT_APP_FRONT_URL;
+
 const corsOptions ={
-    origin:'*',
+    origin: APP_FRONT_URL,
     credentials:true,
     optionSuccessStatus:200,
 }
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+
+const data = {
+    message: "API 호출이 성공적으로 처리되었습니다.",
+    status: "success"
+};
+
+app.get('/api', (req, res) => {
+    // CORS 헤더 설정
+    res.header("Access-Control-Allow-Origin", APP_FRONT_URL);
+    res.header("Access-Control-Allow-Credentials", "true");
+    
+    // data를 응답으로 보냄
+    res.send(data);
+})
+
+// 서버 실행
+app.listen(5000, () => {
+    console.log('서버가 포트 5000에서 실행 중입니다.');
+});
